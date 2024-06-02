b846f49b33ff09327e0f3bc637071d594b1f92d6
shradhagupta21 <86425639+shradhagupta21@users.noreply.github.com>
2024-06-02 02:19:31 -0500

    simple rest endpoint that makes call to the rag service implemented with gob routines

--- go.mod
+++ go.mod
@@ -5,4 +5,11 @@ go 1.22.0
 require (
 	github.com/go-chi/chi/v5 v5.0.12
 	github.com/go-chi/cors v1.2.1
+	github.com/parnurzeal/gorequest v0.3.0
+)
+
+require (
+	github.com/moul/http2curl v1.0.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/net v0.25.0 // indirect
 )
--- main.go
+++ main.go
@@ -6,27 +6,33 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/cors"
+	"github.com/parnurzeal/gorequest"
 )
 
 func main() {
 	r := chi.NewRouter()
-    r.Use(cors.Handler(cors.Options{
-        AllowedOrigins: []string{"https://*", "http://*"},
-        AllowedMethods: []string{"PUT","POST","GET","DELET"},
-        AllowedHeaders: []string{"*"},
-        AllowCredentials: false,
-        MaxAge: 300,
-    }))
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"PUT", "POST", "GET", "DELET"},
+		AllowedHeaders:   []string{"*"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("welcome"))
 	})
-
+	request := gorequest.New()
+	myStruct := httpClientStruct{
+		Request: request,
+	}
 	port := ":8080"
 
+	mountedRouter := chi.NewRouter()
+	mountedRouter.Get("/health", handlerReadiness)
 
-    mountedRouter := chi.NewRouter()
-  	mountedRouter.Get("/health", handlerReadiness)
 	r.Mount("/v1", mountedRouter)
+    r.Post("/about",myStruct.request_handler)
 
 	srv := &http.Server{
 		Handler: r,
